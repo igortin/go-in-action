0d5a4f7ccfc86cda386037cb99ec087450e22373
Igor Tin <itin@dar.kz>
2021-03-13 23:30:21 +0600

    Append comments channel-like-mutex

--- chapter3/test/channel-like-mutex/main.go
+++ chapter3/test/channel-like-mutex/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-
+	// Create buffered channel
 	done := make(chan os.Signal, 1)
 
 	signal.Notify(done, os.Interrupt)
@@ -16,7 +16,16 @@ func main() {
 	chLocker := make(chan bool, 1)
 
 	for i := 0; i < 2; i++ {
-		// 7 x Goroutines
+
+		/*
+			id 0 send value to chan chLocker - true and locked it (like Mutex)
+			id 0 succefully start bussines logic
+			id 0 succefully do step by step bussiness logic
+			id 1 send value to chan chLocker but hang until id 0 will release lock (buffer fulled)
+			id 0 succesfully completed bussiness logic
+			id 0 release lock
+			id 1 succesfully started bussiness logic
+		*/
 		go func(id int, c chan bool) {
 			fmt.Printf("id #%v: wants Lock\n", id)
 			c <- true
